8f694738a00d14f88ed3039a4397f2eb38627c51
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-09-25 20:37:19 -0300

    Absolute faliure

--- ChainmailGodot/ChainmailManager.gd
+++ ChainmailGodot/ChainmailManager.gd
@@ -1,42 +1,141 @@
 extends Spatial
 
-onready var vol = preload("Volume.gd")
+const HOR_DIST = 1
+const VER_DIST = 1
+const LINELEN = sqrt(HOR_DIST*HOR_DIST + VER_DIST * VER_DIST)
+
+var points = []
+var sticks = []
+var bounce = 0.9
+var gravity = -0.001
+var friction = 0.999
+
+class Point:
+	var isStatic: bool
+	var center: bool
+	var mesh: MeshInstance
+	var currentPos: Vector3
+	var oldPos: Vector3
+	func _init(isStatic: bool, center: bool, currentPos: Vector3, oldPos: Vector3):
+		self.isStatic = isStatic
+		self.center = center
+		self.currentPos = currentPos
+		self.oldPos = oldPos
+
+class Stick:
+	var p0: Point
+	var p1: Point
+	var length: float
+	
+	func _init(p0: Point, p1: Point, length: float):
+		self.p0 = p0
+		self.p1 = p1
+		self.length = length
+		
 var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
 	
-	var size = vol.Vector3i.new(9, 9, 9)
-	var volume = vol.Volume.new(gen_data(size), size, Vector3(0.6, 0.6, 0.6), Vector3(0.2, 0.2, 0.2))
-	var deformation = Vector3(1, 1, 3)
-	var deformation_index = vol.Vector3i.new(7, 7, 8)
-	volume.deform(deformation_index, deformation)
-	
-	var positions = []
-	for i in range(size.x):
-		for j in range(size.y):
-			for k in range(size.z):
-				positions.append(volume.get_position(vol.Vector3i.new(i, j, k)))
+	generate_lattice(20, 20)
 
-	for chainLink in positions:
-		var mesh := MeshInstance.new()
-		mesh.mesh = CubeMesh.new()
-		mesh.mesh.size = Vector3(1, 1, 1)
-		mesh.translate(chainLink)
-		add_child(mesh)
+	for chainLink in points:
+		var mesh1 := MeshInstance.new()
+		mesh1.mesh = SphereMesh.new()
+		mesh1.mesh.radius = 0.1
+		mesh1.mesh.height = 0.1
+		mesh1.translate(chainLink.currentPos)
+		add_child(mesh1)
+		chainLink.mesh = mesh1
 
-func gen_data(size) -> Array:
-	var data = []
-	for x in range(size.x):
-		data.append([])
-		for y in range(size.y):
-			data[x].append([])
-			for z in range(size.z):
-				data[x][y].append(Vector3(rng.randf_range(0, 1), rng.randf_range(0, 1), rng.randf_range(0, 1)))
-	return data
+		"""
+		if chainLink.center:
+			var mesh := MeshInstance.new()
+			mesh.mesh = load("res://torus.obj") 
+			mesh.translate(chainLink.currentPos)
+			add_child(mesh)
+			chainLink.mesh = mesh
+		"""
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	for i in range(len(points)):
+		points[i].currentPos = points[i].mesh.transform.origin
+	
+	updatePoints()
+	for i in range(10):
+		updateSticks()
+	
+	for i in range(len(points)):
+		points[i].mesh.transform.origin = points[i].currentPos
 
+func updatePoints():
+	for i in range(len(points)):
+		var p = points[i]
+		var v: Vector3 = (p.currentPos - p.oldPos) * friction
+		if(p.isStatic):
+			continue
+	
+		p.oldPos = p.currentPos
+		p.currentPos += v
+		p.currentPos.y += gravity
+
+func updateSticks():
+	for i in range(len(sticks)):
+		var s = sticks[i]
+		var d = s.p1.currentPos - s.p0.currentPos
+		var distance = sqrt(d.x * d.x + d.y * d.y) + 0.0001
+		var difference = min(s.length - distance, 0)
+		var percent = difference / distance / 2
+		var offsetX = d.x * percent
+		var offsetY = d.y * percent
+		
+		if(!s.p0.isStatic && !s.p1.isStatic):
+		  s.p0.currentPos.x -= offsetX
+		  s.p0.currentPos.y -= offsetY
+		  s.p1.currentPos.x += offsetX
+		  s.p1.currentPos.y += offsetY
+		elif(s.p0.isStatic):
+		  s.p1.currentPos.x += 2 * offsetX
+		  s.p1.currentPos.y += 2 * offsetY
+		elif(s.p1.isStatic):
+		  s.p0.currentPos.x -= 2 * offsetX
+		  s.p0.currentPos.y -= 2 * offsetY
+		
+		
+func generate_lattice(width, height):
+	var prevTop = []
+
+	for i in range(width):
+		var pos = self.translation + Vector3(HOR_DIST*i, 0, 0)
+		var point = Point.new(false, false, pos, pos)
+	  
+		prevTop.append(point)
+		points.append(point)
+
+	for j in range(height):
+		var topPoints = prevTop
+		var bottomPoints = []
+		for i in range(width):
+			var pos = self.translation + Vector3(HOR_DIST*i, (1 + j) * VER_DIST, 0)
+			var point = Point.new(i == 0 || i == width - 1, false, pos, pos)
+			bottomPoints.append(point)
+			points.append(point)
+		
+		for i in range(width - 1):
+			var pos = self.translation + Vector3(HOR_DIST*i + HOR_DIST/2, j * VER_DIST + VER_DIST/2, 0)
+			var middlePoint = Point.new(false, true, pos, pos)
+		
+			points.append(middlePoint)
+		  
+			sticks.append(Stick.new(topPoints[i], middlePoint, LINELEN))
+			sticks.append(Stick.new(topPoints[i+1], middlePoint, LINELEN))
+			sticks.append(Stick.new(bottomPoints[i], middlePoint, LINELEN))
+			sticks.append(Stick.new(bottomPoints[i+1], middlePoint, LINELEN))
+		
+		prevTop = bottomPoints
+		
+	for point in prevTop:
+		point.isStatic = true
+	
